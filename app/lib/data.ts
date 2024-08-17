7c2b9f6206b73b2f6cbe8d8a3100cb4edca1b3a0
Tao Ning <ningtaous@gmail.com>
2024-08-17 05:32:15 -0500

    Implement data query for the invoice and revenue card

---
+++ app/lib/data.ts
@@ -0,0 +1,33 @@
+import { sql } from "@vercel/postgres";
+import { LatestInvoiceRaw, Revenue } from "@/app/lib/definitions";
+import { formatCurrency } from "@/app/lib/utils";
+
+export async function fetchRevenue() {
+  try {
+    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch revenue data.");
+  }
+}
+
+export async function fetchLatestInvoices() {
+  try {
+    const data = await sql<LatestInvoiceRaw>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
+
+    const latestInvoices = data.rows.map((invoice) => ({
+      ...invoice,
+      amount: formatCurrency(invoice.amount),
+    }));
+    return latestInvoices;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch the latest invoices.");
+  }
+}
