fcc3c63982e3e4ec6656ad74f584d049d4d81735
Tao Ning <ningtaous@gmail.com>
2024-09-13 03:23:16 -0500

    [feat] Implement GitHub login

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,41 +1,19 @@
 import NextAuth from "next-auth";
-import { authConfig } from "@/app/lib/auth.config";
-import Credentials from "next-auth/providers/credentials";
-import { z } from "zod";
-import { sql } from "@vercel/postgres";
-import type { User } from "@/app/lib/definitions";
-import bcrypt from "bcrypt";
+import GitHub from "next-auth/providers/github";
 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
-  }
-}
-
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [GitHub],
 });
--- middleware.js
+++ middleware.js
@@ -1,7 +1,4 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/app/lib/auth.config";
-
-export default NextAuth(authConfig).auth;
+export { auth as default } from "@/app/lib/auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.JPG$).*)"],
