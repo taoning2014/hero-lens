b93f95912f0cf54496e584421dfe19de006df538
Tao Ning <ningtaous@gmail.com>
2024-09-15 09:10:01 -0500

    [feat] Implement Google login (#26)
    * Add logo to the login
    
    * Google sign in

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  theme: {
+    logo: "https://hero-lens.vercel.app/_next/image?url=%2Flogo.JPG&w=1080&q=75",
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
@@ -15,5 +19,5 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return true;
     },
   },
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
