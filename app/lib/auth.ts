839631a74f09169bf0b3284addebb794be7bcbad
Tao Ning <ningtaous@gmail.com>
2024-09-15 08:43:09 -0500

    Add logo to the login

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  theme: {
+    logo: "https://hero-lens.vercel.app/_next/image?url=%2Flogo.JPG&w=1080&q=75",
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
