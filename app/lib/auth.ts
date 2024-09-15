02653c3ff189b9ca743f2d077dc1c5057cca7a8f
Tao Ning <ningtaous@gmail.com>
2024-09-15 09:04:17 -0500

    Google sign in

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   theme: {
@@ -18,5 +19,5 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return true;
     },
   },
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
